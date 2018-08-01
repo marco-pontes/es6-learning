e300cb8f9a47e877e28dbe5313f25933ef05bf36
marco-pontes <marco.pontes@resultadosdigitais.com>
2018-07-31 23:40:16 -0300

    Date, map, spread

--- client/js/app/controllers/negociacao-controller.js
+++ client/js/app/controllers/negociacao-controller.js
@@ -12,8 +12,20 @@ class NegociacaoController {
         event.preventDefault();
         console.log(typeof this._inputData.value);
         console.log(this._inputData.value);
+        let data = new Date(
+            ...this._inputData.value
+                .split('-')
+                .map(function (item, index) {
+                    if(index == 1) {
+                        return item - 1;
+                    }
+                    return item;
+                })
+        );
+        console.log(data);
+
         let negociacao = new Negociacao(
-            this._inputData.value,
+            data,
             this._inputQuantidade.value,
             this._inputValor.value
         );
