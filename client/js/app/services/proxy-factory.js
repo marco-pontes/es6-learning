dee39533384d50241bf01e1c2e7ada0e53fbd3d2
marco-pontes <marco.pontes@resultadosdigitais.com>
2018-08-12 02:51:21 -0300

    Proxy factory adjustment to watch property assignment

--- client/js/app/services/proxy-factory.js
+++ client/js/app/services/proxy-factory.js
@@ -1,17 +1,30 @@
 class ProxyFactory {
     static create(objeto, props, acao) {
-        return new Proxy(new ListaNegociacoes(), {
+        return new Proxy(objeto, {
             get(target, prop, receiver) {
 
-                if(props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                if(props.includes(prop) && ProxyFactory.isFunction(target[prop])) {
                     return function () {
                         console.log(`Interceptando function: ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        return acao(target);;
+                        let result = Reflect.apply(target[prop], target, arguments);
+                        acao(target);
+                        return result;
                     }
                 }
                 return Reflect.get(target, prop, receiver);
+            },
+            set(target, prop, value, receiver) {
+                console.log(`Interceptando function(set): ${prop}`);
+                let result = Reflect.set(target, prop, value, receiver);
+                if(props.includes(prop)) {
+                    acao(target);
+                }
+                return result;
             }
         });
+    }
+
+    static isFunction(func) {
+        return typeof(func) == typeof(Function);
     }
 }
