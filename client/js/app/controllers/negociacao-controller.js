87eae432da05bacba0f6dc8bb76ca98e28aa93fc
marco-pontes <marco.pontes@resultadosdigitais.com>
2018-08-12 20:18:07 -0300

    Importa negociações, AJAX

--- client/js/app/controllers/negociacao-controller.js
+++ client/js/app/controllers/negociacao-controller.js
@@ -33,6 +33,26 @@ class NegociacaoController {
         this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
 
+    importaNegociacoes () {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/semanax');
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4) {
+                if(xhr.status == 200) {
+                    console.log(`Obtendo as negociações do servidor.`);
+                    let negociacoes = JSON.parse(xhr.responseText)
+                        .map((item) => new Negociacao(new Date(item.data), item.quantidade, item.valor))
+                        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = 'Negociações importadas com sucesso.';
+                } else {
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possível obter as negociações.';
+                }
+            }
+        };
+        xhr.send();
+    }
+
     criaNegociacao(){
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
