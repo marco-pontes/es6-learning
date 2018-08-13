fca06d3438f70af3d7e07375c12dec82c2150449
marco-pontes <marco.pontes@resultadosdigitais.com>
2018-08-13 02:10:06 -0300

    HttpService refactor to return the same promise

--- client/js/app/services/negociacao-service.js
+++ client/js/app/services/negociacao-service.js
@@ -5,39 +5,33 @@ class NegociacaoService {
     }
 
     obterNegociacoesDaSemana() {
-        return new Promise((resolve, reject) => {
-            this._http.get('negociacoes/semana').then(json => {
+        return this._http.get('negociacoes/semana').then(json => {
                 let negociacoes = json.map((item) => new Negociacao(new Date(item.data), item.quantidade, item.valor));
-                resolve(negociacoes);
+                return negociacoes;
             }).catch(erro => {
                 console.log(erro);
-                reject('Não foi possível obter as negociações da semana');
+                throw new Error('Não foi possível obter as negociações da semana');
             });
-        });
     }
 
     obterNegociacoesDaSemanaAnterior() {
-        return new Promise((resolve, reject) => {
-            this._http.get('negociacoes/anterior').then(json => {
+        return this._http.get('negociacoes/anterior').then(json => {
                 let negociacoes = json.map((item) => new Negociacao(new Date(item.data), item.quantidade, item.valor));
-                resolve(negociacoes);
+                return negociacoes;
             }).catch(erro => {
                 console.log(erro);
-                reject('Não foi possível obter as negociações da semana anterior');
+                throw new Error('Não foi possível obter as negociações da semana anterior');
             });
-        });
     }
 
     obterNegociacoesDaSemanaRetrasada() {
-        return new Promise((resolve, reject) => {
-            this._http.get('negociacoes/retrasada').then(json => {
+        return this._http.get('negociacoes/retrasada').then(json => {
                 let negociacoes = json.map((item) => new Negociacao(new Date(item.data), item.quantidade, item.valor));
-                resolve(negociacoes);
+                return negociacoes;
             }).catch(erro => {
                 console.log(erro);
-                reject('Não foi possível obter as negociações da semana retrasada');
+                throw new Error('Não foi possível obter as negociações da semana retrasada');
             });
-        });
     }
 
     salva(objeto, cb) {
