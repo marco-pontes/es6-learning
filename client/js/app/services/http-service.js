1a85961f8bd9122607be8c622ea68aa4cd4e026e
marco-pontes <marco.pontes@resultadosdigitais.com>
2018-08-21 00:08:21 -0300

    Fetch API

--- client/js/app/services/http-service.js
+++ client/js/app/services/http-service.js
@@ -1,39 +1,25 @@
 class HttpService {
 
+    handleErrors(result) {
+        if(!result.ok) {
+            throw new Error(result.status.text());
+        }
+        return result;
+
+    }
+
     get(url) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4) {
-                    if(xhr.status == 200) {
-                        let result = JSON.parse(xhr.responseText);
-                        resolve(result);
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send();
-        });
+        return fetch(url)
+            .then(res => this.handleErrors(res))
+            .then(res => res.json());
     }
 
     post(url, dado) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado));
-        });
+        return fetch(url, {
+            headers: {'Content-type': 'application/json'},
+            method: 'post',
+            body: JSON.stringify(dado)
+        }).then(res => this.handleErrors(res));
     }
 
 }
